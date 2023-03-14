1d10a9e96be2c5bd3e744f88dc5332035590a4c4
Pascalm <PHerderich@hotmail.com>
2023-03-15 00:18:26 +0100

    Added Extenion Method for IService Collection

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Collections.Generic;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<List.Handler>());
+
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,36 +1,13 @@
-using Application.Activities;
-using Application.Core;
-using Microsoft.AspNetCore.Hosting;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-// Add DB Context Service & give it a connection String from the appsettings
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<List.Handler>());
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
