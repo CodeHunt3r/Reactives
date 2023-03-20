393cf054d7695805595f22de6c5f01ea408891ad
Pascalm <PHerderich@hotmail.com>
2023-03-21 00:30:58 +0100

    Added Input From for Activity

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 interface Props {
     activites: Activity[];
 }
 
-export default function ActivityDashboard({activites}: Props) {
+export default function ActivityDashboard({ activites }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-               <ActivityList activites={activites}/>
+                <ActivityList activites={activites} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activites[0] &&
+                    <ActivityDetails activity={activites[0]} />}
+                <ActivityForm />
             </Grid.Column>
         </Grid>
     )
